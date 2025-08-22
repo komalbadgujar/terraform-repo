648b21f4c59f3a44067e866638d240e1fde8d4e4
komalbadgujar <komalbadgujar1057@gmail.com>
2025-08-22 15:56:38 +0530

    added

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my first_instance" {
-    ami = "ami-00ca32bbc84273381"
+    ami = "i-08b0bc14fe7ef3ac7"
     instance_type = "t2.micro"
     tags = {
       Name = "webserver"
