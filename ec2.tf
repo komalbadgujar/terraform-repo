7a0b3396953ba28b98485fe0a0d8818115f33bb8
komalbadgujar <komalbadgujar1057@gmail.com>
2025-08-22 16:01:36 +0530

    added

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,7 @@ provider "aws" {
   region = "us-east-1"
 
 }
-
-resource "aws_instance" "my first_instance" {
+resource "aws_instance" "my first instance" {
     ami = "i-08b0bc14fe7ef3ac7"
     instance_type = "t2.micro"
     tags = {
